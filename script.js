89844fba4e314b46c5e850120690c50a17ff82bb
sekon-am <sekon.am@gmail.com>
2016-01-20 22:12:22 +0200

    Mixer player done

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-function Player(element, videoUrl, frameAmount, onloadCallback) 
+function Player(element, videoUrl, frameAmount) 
 {
 	this.DELAY = 150;
 	this.REPLAY = false;
 	this.frame = {};
 	this.element = element;
+	this.counter = 0;
+	this.frameAmount = frameAmount;
+	this.onload = null;
+	this.onend = null;
+	this.src = videoUrl;
 	
 	var self = this,
-		counter = 0,
 		img = null,
 		pause = true,
 		frame = this.frame;
 
 	function run()
 	{
-		element.style.backgroundPosition = '0px ' + (-counter * frame.height) + 'px';
+		element.style.backgroundPosition = '0px ' + (-self.counter * frame.height) + 'px';
 		
-		if (++counter >= frameAmount) {
-			counter = 0;
+		if (++self.counter >= frameAmount) {
+			self.counter = 0;
 			
 			if (!self.REPLAY) {
 				self.stop();
+		
+				if (self.onend && {}.toString.call(self.onend) === '[object Function]') {
+					self.onend.call(self);
+				}
+				
 			}
 		}
 		
@@ -56,30 +65,108 @@ function Player(element, videoUrl, frameAmount, onloadCallback)
 			element.style[field] = style[field];
 		}
 		
-		if (onloadCallback && {}.toString.call(onloadCallback) === '[object Function]') {
-			onloadCallback.call(self);
+		if (self.onload && {}.toString.call(self.onload) === '[object Function]') {
+			self.onload.call(self);
 		}
-		
-		self.start();
 	};
-	img.src = videoUrl;
+	this.load = function () {
+		img.src = videoUrl;
+	};
 }
 
-window.onload = function () {
-	var mainStream = Player(document.querySelector('.main-stream .player'), 'sprite1xx.jpeg', 100);
-	for (var i=1; i<=4; i++) {
-		(function (i) {
-			var player = new Player(document.querySelector('.stream:nth-child(' + i + ') .player'), 
-				'sprite' + i + 'xx.jpeg', 
-				100, 
-				function () {
+function Mixer()
+{
+	this.onstop = null;
+	
+	var players = [],
+		mainPlayer = null,
+		self = this;
+
+	function init()
+	{
+		mainPlayer = new Player(document.querySelector('.main-stream .player'), 'sprite1xx.jpeg', 100, true);
+		mainPlayer.onend = function () {
+			self.stop();
+		};
+		
+		for (var i=1; i<=4; i++) {
+			(function (i) {
+				var player = new Player(
+					document.querySelector('.stream:nth-child(' + i + ') .player'), 
+					'sprite' + i + 'xx.jpeg', 
+					100
+				);
+				player.onload = function () {
 					var pw = document.querySelector('.stream:nth-child(' + i + ')'),
 						pwH3 = pw.querySelector('h3'),
 						pwH3style = pwH3.currentStyle || window.getComputedStyle(pwH3);
 						pwHeight = parseFloat(pwH3style.marginTop) + parseFloat(pwH3style.height) + parseFloat(pwH3style.marginBottom) + player.frame.height * player.frame.zoom;
 					pw.style.height = pwHeight + 'px';
-				}
-			);
-		})(i);
+				};
+				players.push(player);
+			})(i);
+		}
+	} init();
+	
+	function load() {
+		mainPlayer.load();
+		players.map(function (player) {
+			player.load();
+		});
+	} load();
+	
+	function scroll() {
+		if (mainPlayer) {
+			var shift = document.querySelector('.record-progress').clientWidth / mainPlayer.frameAmount;
+			document.querySelector('.record-progress .indicator').style.left = shift * mainPlayer.counter + 'px';
+			if (self.played) {
+				setTimeout(scroll, mainPlayer.DELAY);
+			}
+		}
 	}
+	
+	this.played = false;
+
+	this.start = function () {
+		this.played = true;
+		mainPlayer.start();
+		players.map(function (player) {
+			player.start();
+		});
+		scroll();
+	};
+
+	this.stop= function () {
+		this.played = false;
+		mainPlayer.stop();
+		players.map(function (player) {
+			player.stop();
+		});
+		if (self.onstop && {}.toString.call(self.onstop) === '[object Function]') {
+			self.onstop.call(self);
+		}
+	};
+	
+	this.play = function () {
+		
+		if (this.played) {
+			this.stop();
+		} else {
+			this.start();
+		}
+		
+		return this.played;
+	};
+}
+
+window.onload = function () {
+	var mixer = new Mixer;
+	mixer.onstop = function () {
+		document.querySelector('.buttons .play').innerHTML = 'Start';
+	};
+	document.querySelector('.buttons .play').addEventListener('click', function () {
+		if (mixer.play()) {
+			document.querySelector('.buttons .play').innerHTML = 'Stop';
+		}
+	});
 };
